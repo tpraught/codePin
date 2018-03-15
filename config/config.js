d69fa405745188ce4c57cc949922b3f23e1352d2
Nathan Jahnke <naterased@gmail.com>
2018-03-15 00:41:58 -0500

    added .env file for database connection.

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
-{
+require('dotenv').config(); // this is important!
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "codePin",
-    "host": "localhost",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
@@ -20,4 +21,4 @@
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
-}
+};
