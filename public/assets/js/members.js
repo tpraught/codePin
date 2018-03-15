7875fe63c04d9f25bf091963ebc7798637d4e593
Elizabeth <erischjanson@gmail.com>
2018-03-15 17:40:00 -0500

    modal displaying with fields

--- public/assets/js/members.js
+++ public/assets/js/members.js
@@ -4,5 +4,30 @@ $(document).ready(function() {
     $.get("/api/user_data").then(function(data) {
       $(".member-name").text(data.email);
     });
+
+    // on-click function on the Add Pin button to display the New Pin modal
+
+
+    //add button to create new pin
+
+    $("#addPin").on("click", function(event){
+      event.preventDefault();
+      $("#newPinModal").modal("toggle");
+    });
+
+    $("#submit").on("click", function(event){
+
+      event.preventDefault();
+
+      //grab all the form data and put into newPin object
+      var newPin = {
+        $("#name").val().trim(),
+        $("#description").val().trim(),
+        $("#language").val().trim(),
+        $("#snippet").val().trim()
+      };
+      console.log(newPin);
+    });
+
   });
   
