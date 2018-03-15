99e97b43ab397ec45c21837220285259c2dcdf0f
Leif Swenson <leifswenson@gmail.com>
2018-03-15 15:35:40 +0000

    added api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,15 +1,97 @@
 // Requiring models
-// =============================================================
+// ===============================================================================================
 var db = require("../models");
 
 module.exports = function(app) {
 
-  // GET route for getting all pins
+
+  // GET route for getting all pins.
+  // ===============================================================================================
   app.get("/api/pins", function(req, res) {
     
-    db.Todo.findAll({}).then(function(dbPin) {
+    db.Pin.findAll({}).then(function(dbPin) {
       res.json(dbPin);
     });
-  }); // end of get
+  }); 
+  
+  
+  // GET rotue for retrieving a single pin.
+  // ===============================================================================================
+  app.get("/api/pins/:id", function(req, res) {
+    db.Pin.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(function(dbPin) {
+      res.json(dbPin);
+    });
+  });
+  
+  
+  // GET rotue for returning pins of a specific language
+  // ===============================================================================================
+  app.get("/api/pins/language/:language", function(req, res) {
+    db.Pin.findAll({
+      where: {
+        language: req.params.language
+      }
+    })
+    .then(function(dbPin) {
+      res.json(dbPin);
+    });
+  });
 
-}; //end of export
+
+  // POST route for saving a new pin.
+  // ===============================================================================================
+  app.post("/api/pins", function(req, res) {
+    
+    db.Pin.create({
+      title: req.body.title,
+      description: req.body.description,
+      language: req.body.language,
+      media_type: req.body.media_type,
+      content: req.body.content
+    }).then(function(dbPin) {
+      res.json(dbPin);
+    });
+  }); 
+  
+  
+   // DELETE route for deleting pins.
+   // ===============================================================================================
+  app.delete("/api/pins/:id", function(req, res) {
+    
+    db.Pin.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbPin) {
+      res.json(dbPin);
+    });
+
+  }); 
+
+
+  // PUT route for updating pins
+  // ===============================================================================================
+  app.put("/api/pins", function(req, res) {
+    
+    db.Pin.update({
+      title: req.body.title,
+      description: req.body.description,
+      language: req.body.language,
+      media_type: req.body.media_type,
+      content: req.body.content
+    }, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbPin) {
+      res.json(dbPin);
+    });
+  });
+
+
+};
