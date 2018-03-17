53ca4df5c7cbe3250e82412b90261d8a98850450
Praught <tpraught@osii.com>
2018-03-17 11:56:57 -0500

    add login and signup routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,6 +7,12 @@ module.exports = function(app) {
     res.render("index");
   });
 
+  app.get("/login", function(req, res) {
+    res.render("login");
+  });
 
+  app.get("/signup", function(req, res) {
+    res.render("signUp");
+  });
 
 }
